d596568dd9a5325cfa3f6dfe4373d1c391647ef1
AbdallahElhosary <114493396+AbdallahElhosary@users.noreply.github.com>
2022-11-28 16:50:15 +0200

    Hello

---
+++ master.js
@@ -0,0 +1,9 @@
+let img = document.querySelector(".img");
+let footer = document.querySelector("footer");
+function phones(phoneSrc) {
+    img.src = phoneSrc;
+}
+function Colors(color) {
+    document.body.style.backgroundColor = color;
+    footer.style.backgroundColor = color;
+}
